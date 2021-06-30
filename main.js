ea0e69b35ca7b1974a281a1c38b54a23acb8a5dd
Max Siegel <maxsiegel481@gmail.com>
2021-06-30 14:59:55 -0400

    add laser, update

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload(impath) {
+    var img = new Image();
+    img.src = impath;
+}
+
+preload('laser.png');
+
+var html = document.querySelector("html");
+
+function laser() {
+    html.style.cursor = "url('laser.png'), auto";
+}
+function brain() {
+    // document.body.style.cursor = "url('cursor.png'), auto";
+    html.style.cursor = "url('cursor.png'), auto";
+}
+
+html.onmousedown = laser;
+html.onmouseup = brain;
