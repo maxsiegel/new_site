011535a603994339bfdc697c08ed774523e53ad9
Max Siegel <maxs@mit.edu>
2026-02-12 21:13:51 -0600

    _

--- main.js
+++ main.js
@@ -18,3 +18,14 @@ function brain() {
 
 html.onmousedown = laser;
 html.onmouseup = brain;
+
+function loadHTML(id, file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        });
+}
+
+loadHTML("header", "/header.html");
+// loadHTML("footer", "/footer.html");
